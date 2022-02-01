fc4de813edbf221736ed0208b71b30c4514f24c3
Alan Yu <kaisite2004@gmail.com>
2022-02-01 21:13:26 +0800

    Added input debounced & search function

--- src/park-list/park-list.jsx
+++ src/park-list/park-list.jsx
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import parksData from "../assets/parks.json";
+import { useDebouncedEffect } from "../useDebouncedEffect";
 import "./park-list.scss";
 
 function ParkList() {
     const navigate = useNavigate();
+
     const [loading, setLoading] = useState(true);
     const [currPage, setCurrPage] = useState(1);
-    const parks = parksData;
+    const [highlightIndex, setHighlightIndex] = useState([]);
+    const [search, setSearch] = useState("");
+    useDebouncedEffect(() => searchOnchange(), [search], 500);
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+
+    let totalPage;
+    let parks = parksData;
     const cardPerPage = 12;
     const len = parks.length / cardPerPage;
-    let totalPage;
 
     if (len % 1 === 0) {
         totalPage = len;
@@ -18,13 +27,9 @@ function ParkList() {
         totalPage = Math.floor(len) + 1;
     }
 
-    useEffect(() => {
-        setLoading(false);
-    }, []);
-
     function loadPage(action) {
-        // this.highlightIndex = [];
-        // this.input.nativeElement.value = '';
+        setHighlightIndex([]);
+        setSearch("");
         if (action === "next" && currPage !== totalPage) {
             setCurrPage((prev) => prev + 1);
         } else if (action === "prev" && currPage !== 1) {
@@ -38,6 +43,25 @@ function ParkList() {
 
     function cardClick(index) {
         navigate("/park-view/" + index);
+    }
+
+    function searchOnchange() {
+        setHighlightIndex([]);
+        if (search === "") return;
+        const subParks = parks.slice((currPage - 1) * 12, currPage * 12);
+        subParks.filter((obj, i) => {
+            if (
+                obj["pm_name"].includes(search) ||
+                obj["pm_construction"].includes(search) ||
+                obj["pm_overview"].includes(search)
+            ) {
+                setHighlightIndex((highlightIndex) => [...highlightIndex, i]);
+                i++;
+                return obj;
+            }
+            i++;
+        });
+        // console.log(search, highlightIndex, highlightIndex.includes(0));
     }
 
     return (
@@ -54,14 +78,22 @@ function ParkList() {
                         type="text"
                         className="input-search"
                         placeholder="Search"
+                        value={search}
+                        onChange={(e) => {
+                            setSearch(e.target.value);
+                        }}
                     />
                     <div className="card-columns">
                         {parks
                             .slice((currPage - 1) * 12, currPage * 12)
                             .map((r, index) => (
                                 <div
-                                    className="card no-highlight"
-                                    key={r.id}
+                                    className={
+                                        highlightIndex.includes(index)
+                                            ? "card highlight"
+                                            : "card no-highlight"
+                                    }
+                                    key={index}
                                     onClick={() =>
                                         cardClick((currPage - 1) * 12 + index)
                                     }
