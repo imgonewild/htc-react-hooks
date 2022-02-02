f1128042d96ba2385612f5bd625cec852ed68a68
Alan Yu <kaisite2004@gmail.com>
2022-02-03 00:02:52 +0800

    Added local/global searching function & save info to localStorage

--- src/park-list/park-list.jsx
+++ src/park-list/park-list.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import parksData from "../assets/parks.json";
@@ -6,92 +8,135 @@ import "./park-list.scss";
 
 function ParkList() {
     const navigate = useNavigate();
-
     const [loading, setLoading] = useState(true);
     const [currPage, setCurrPage] = useState(1);
+    const [totalPage, setTotalPage] = useState();
     const [highlightIndex, setHighlightIndex] = useState([]);
     const [search, setSearch] = useState("");
-    useDebouncedEffect(() => searchOnchange(), [search], 500);
-    useEffect(() => {
-        setLoading(false);
-    }, []);
-
-    let totalPage;
-    //let parks = parksData;
+    const [isSearch, setIsSearch] = useState(false);
+    const [searchMethod, setSearchMethod] = useState("local");
     const [parks, setParks] = useState(parksData);
     const cardPerPage = 12;
     const len = parks.length / cardPerPage;
 
-    if (len % 1 === 0) {
-        totalPage = len;
-    } else {
-        totalPage = Math.floor(len) + 1;
-    }
+    useDebouncedEffect(() => searchOnchange(), [search], 250);
+
+    useEffect(() => {
+        setLoading(false);
+        const search_key = localStorage.getItem("search_key");
+        if (search_key !== null) setSearch(search_key);
+
+        const search_method = localStorage.getItem("search_method");
+        if (search_method !== null) setSearchMethod(search_method);
+
+        const currentPage = localStorage.getItem("current_page");
+        if (currentPage !== null) setCurrPage(currentPage);
+
+        if (len % 1 === 0) {
+            setTotalPage(len);
+        } else {
+            setTotalPage(Math.floor(len) + 1);
+        }
+    }, []);
+
+    useEffect(() => {
+        searchOnchange();
+    }, [searchMethod, currPage]);
 
     function loadPage(action) {
-        setHighlightIndex([]);
-        setSearch("");
         if (action === "next" && currPage !== totalPage) {
-            setCurrPage((prev) => prev + 1);
-        } else if (action === "prev" && currPage !== 1) {
-            setCurrPage((prev) => prev - 1);
-        } else if (action === "start") {
-            setCurrPage(1);
-        } else if (action === "end") {
+            setCurrPage((next) => parseInt(next) + 1);
+        } else if (action === "end" && currPage !== totalPage) {
             setCurrPage(totalPage);
+        } else if (action === "prev" && currPage !== 1) {
+            setCurrPage((prev) => parseInt(prev) - 1);
+        } else if (action === "start" && currPage !== 1) {
+            setCurrPage(1);
+        } else {
+            return;
         }
+        setHighlightIndex([]);
     }
 
     function searchOnchange() {
         setHighlightIndex([]);
-        if (search === "") return;
+        if (search === "" || search.length === 1) {
+            localStorage.removeItem("search_key");
+            localStorage.removeItem("current_page");
+            setParks(() => parksData);
+            setIsSearch(false);
+            return;
+        }
 
-        // setParks(() =>
-        //     parks.filter((obj, i) => {
-        //         if (
-        //             obj["pm_name"].includes(search) ||
-        //             obj["pm_construction"].includes(search) ||
-        //             obj["pm_overview"].includes(search)
-        //         ) {
-        //             setHighlightIndex((highlightIndex) => [
-        //                 ...highlightIndex,
-        //                 i,
-        //             ]);
-        //             i++;
-        //             return obj;
-        //         }
-        //         i++;
-        //         return false;
-        //     })
-        // );
-
-        const subParks = parks.slice((currPage - 1) * 12, currPage * 12);
-        subParks.filter((obj, i) => {
-            if (
-                obj["pm_name"].includes(search) ||
-                obj["pm_construction"].includes(search) ||
-                obj["pm_overview"].includes(search)
-            ) {
-                setHighlightIndex((highlightIndex) => [...highlightIndex, i]);
-                i++;
-                return obj;
-            }
-            i++;
-            return false;
-        });
-        // console.log(highlightIndex);
+        if (searchMethod === "local") {
+            setIsSearch(true);
+            const subParks = parksData.slice(
+                (currPage - 1) * 12,
+                currPage * 12
+            );
+            subParks.filter((obj, i) => {
+                if (
+                    obj["pm_name"].includes(search) ||
+                    obj["pm_construction"].includes(search)
+                ) {
+                    setHighlightIndex((highlightIndex) => [
+                        ...highlightIndex,
+                        i,
+                    ]);
+                    return obj;
+                }
+            });
+        } else if (searchMethod === "global") {
+            setIsSearch(true);
+            setCurrPage(1);
+            setParks(() =>
+                parksData.filter((obj, i) => {
+                    if (
+                        obj["pm_name"].includes(search) ||
+                        obj["pm_construction"].includes(search)
+                    ) {
+                        setHighlightIndex((highlightIndex) => [
+                            ...highlightIndex,
+                            i,
+                        ]);
+                        return obj;
+                    }
+                    return false;
+                })
+            );
+        }
     }
 
     return (
         <>
             <div className="park-list-wrapper">
-                <header className="header">
-                    <div className="text-center title">台北市公園資料</div>
-                </header>
-
                 {loading === true && <div className="loader"></div>}
 
                 <div className="container">
+                    <header className="header">
+                        <span className="tab"></span>
+                        <span className="title">台北市公園資料</span>
+                        <label className="switch">
+                            <input
+                                type="checkbox"
+                                checked={
+                                    searchMethod === "global" ? true : false
+                                }
+                                onChange={() => {
+                                    setSearchMethod(() => {
+                                        if (searchMethod === "local")
+                                            return "global";
+                                        else if (searchMethod === "global")
+                                            return "local";
+                                    });
+                                }}
+                            />
+                            <div
+                                className="slider round"
+                                title="search method"
+                            ></div>
+                        </label>
+                    </header>
                     <input
                         type="text"
                         className="input-search"
@@ -102,34 +147,97 @@ function ParkList() {
                         }}
                     />
                     <div className="card-columns">
-                        {parks
-                            .slice((currPage - 1) * 12, currPage * 12)
-                            .map((r, index) => (
-                                <div
-                                    className={
-                                        highlightIndex.includes(index)
-                                            ? "card highlight"
-                                            : "card no-highlight"
-                                    }
-                                    key={index}
-                                    onClick={() =>
-                                        navigate(
-                                            `/park-view/${
-                                                (currPage - 1) * 12 + index
-                                            }`
-                                        )
-                                    }
-                                >
-                                    <div className="header">
-                                        <div className="title">{r.pm_name}</div>
-                                        <div className="remark text-center">
-                                            {r.pm_construction}
+                        {searchMethod === "local" &&
+                            parksData
+                                .slice((currPage - 1) * 12, currPage * 12)
+                                .map((r, index) => (
+                                    <div
+                                        className={
+                                            highlightIndex.includes(index)
+                                                ? "card highlight"
+                                                : "card no-highlight"
+                                        }
+                                        key={index}
+                                        onClick={() => {
+                                            console.log(highlightIndex);
+                                            localStorage.setItem(
+                                                "current_page",
+                                                currPage
+                                            );
+                                            localStorage.setItem(
+                                                "search_key",
+                                                search
+                                            );
+                                            localStorage.setItem(
+                                                "search_method",
+                                                searchMethod
+                                            );
+                                            navigate(
+                                                `/park-view/${
+                                                    (currPage - 1) * 12 + index
+                                                }`
+                                            );
+                                        }}
+                                    >
+                                        <div className="header">
+                                            <div className="title">
+                                                {r.pm_name}
+                                            </div>
+                                            <div className="remark text-center">
+                                                {r.pm_construction}
+                                            </div>
+                                        </div>
+
+                                        <div className="desc">
+                                            {r.pm_overview}
                                         </div>
                                     </div>
+                                ))}
+                        {searchMethod === "global" &&
+                            parks
+                                .slice((currPage - 1) * 12, currPage * 12)
+                                .map((r, index) => (
+                                    <div
+                                        className={
+                                            isSearch === true
+                                                ? "card highlight"
+                                                : "card no-highlight"
+                                        }
+                                        key={index}
+                                        onClick={() => {
+                                            localStorage.setItem(
+                                                "current_page",
+                                                currPage
+                                            );
+                                            localStorage.setItem(
+                                                "search_key",
+                                                search
+                                            );
+                                            localStorage.setItem(
+                                                "search_method",
+                                                searchMethod
+                                            );
+                                            navigate(
+                                                `/park-view/${
+                                                    highlightIndex[index] - 1
+                                                }`
+                                            );
+                                        }}
+                                    >
+                                        <div className="header">
+                                            <div className="title">
+                                                {r.pm_name}
+                                            </div>
+                                            <div className="remark text-center">
+                                                {r.pm_construction}
+                                            </div>
+                                        </div>
 
-                                    <div className="desc">{r.pm_overview}</div>
-                                </div>
-                            ))}
+                                        <div className="desc">
+                                            {r.pm_overview}
+                                        </div>
+                                    </div>
+                                ))}
                     </div>
                 </div>
 
